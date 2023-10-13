2c23fd4de7d44981ec511611b95536a7b85b0d8c
mahadi-init <mahadi.init@proton.me>
2023-10-13 13:50:49 +0600

    update portfolio infos

--- src/data/info.ts
+++ src/data/info.ts
@@ -8,36 +8,7 @@ export const info = {
   I am currently looking for a job as a web developer.
   `,
 
-  experience: [
-    {
-      name: "SkillJobs (Remote)",
-      location: "Dhanmondi, Dhaka, Bangladesh",
-      startDate: "Sep 2022",
-      endDate: "Jan 2023",
-      description: [
-        "- Developing backend service with Java and Java API",
-        "- Building websites with React, Next.js, and Tailwind CSS",
-      ],
-    },
-  ],
-
   education: [
-    {
-      name: "Sarishabari R.D.M ",
-      location: "Sarishabari, Jamalpur, Bangladesh",
-      startDate: "2019",
-      endDate: "2022",
-      description: [
-        "Secondary School Certificate (SSC)",
-      ],
-    },
-    {
-      name: "Modhupur Higher Secondary School",
-      location: "Modhupur, Tangail, Bangladesh",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Higher Secondary Certificate (HSC)"],
-    },
     {
       name: "Daffodil International University",
       location: "Ashulia, Savar, Bangladesh",
@@ -47,28 +18,65 @@ export const info = {
     },
   ],
 
+  skills: ["OOP Knowledge", "Clean Coding", "Design Patterns"],
+  technologies: [
+    {
+      languages: ["HTML", "CSS", "TS", "Java"],
+      frameworks: ["ReactJs", "NextJs"],
+      backend: ["NestJs"],
+      version_control: ["Git", "Github"],
+      database: ["Firebase", "Postgresql"],
+      other: ["Prisma", "Docker"],
+    },
+  ],
+
+  experience: [
+    {
+      name: "SkillJobs (Remote)",
+      location: "Dhanmondi, Dhaka, Bangladesh",
+      startDate: "Sep 2022",
+      endDate: "Jan 2023",
+      description: [
+        "- Build Website Using HTML, CSS, JS and React",
+        "- Worked under Senior guidance",
+        "- Used technologies like ORM to manage databases.",
+      ],
+    },
+  ],
+
   socialMedia: {
-    facebook: "https://www.facebook.com/this.mahadi/",
-    twitter: "https://twitter.com/meMahadihasan",
-    github: "https://github.com/im-mahadi",
-    email: "me.mahadi@outlook.com",
-    linkedin: "",
+    facebook: "https://www.facebook.com/me.mahadi/",
+    github: "https://github.com/mahadi-init",
+    email: "init.mahadi@gmail.com",
+    linkedin: "https://www.linkedin.com/in/mahadi-hasan-637a87240/",
   },
 
   projects: [
     {
-      title: "Code Store",
+      title: "Tunify",
       isFeatured: true,
-      thumbnail: "/assets/projects/code-store.png",
-      githubUrl: "https://github.com/im-mahadi/code-store",
-      liveUrl: "https://code-store.vercel.app/",
+      thumbnail: "/assets/projects/tunify.png",
+      githubUrl: "https://github.com/mahadi-init/tunify",
+      liveUrl: "https://tunify-xi.vercel.app/",
     },
     {
       title: "mh-blog",
       isFeatured: true,
       thumbnail: "/assets/projects/mh-blog.png",
-      githubUrl: "https://github.com/im-mahadi/mh-blog",
+      githubUrl: "https://github.com/mahadi-init/mh-blog",
       liveUrl: "https://mh-blog-three.vercel.app/",
+    },
+    {
+      title: "Wallpaper Changer",
+      isFeatured: true,
+      thumbnail: "/assets/projects/wallpaper.png",
+      githubUrl: "https://github.com/mahadi-init/wallpaper-changer",
+    },
+    {
+      title: "Batch54",
+      isFeatured: true,
+      thumbnail: "/assets/projects/batch54.png",
+      githubUrl: "https://github.com/mahadi-init/Batch54",
     },
   ],
 };
